7d9686f9fab28ce3d5a4b827ee6dd0ac3237ea12
JovanArick <jovancortezche@gmail.com>
2025-05-28 13:53:42 -0500

    Delete Prestamos/admin directory

--- Prestamos/admin/notificaciones_prestamo.php
+++
@@ -1,6 +0,0 @@
-<?php
-function enviarNotificacionPrestamo($pdo, $id_usuario, $titulo, $mensaje) {
-    $stmt = $pdo->prepare("INSERT INTO notificaciones (id_usuario, titulo, mensaje) VALUES (?, ?, ?)");
-    $stmt->execute([$id_usuario, $titulo, $mensaje]);
-}
-?>
